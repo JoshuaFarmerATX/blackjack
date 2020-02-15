9770e6fcd5b9f367e2cdd54276794e74039f8686
JoshuaFarmerATX <53586634+JoshuaFarmerATX@users.noreply.github.com>
2020-02-15 10:35:33 -0600

    Ran and installed black

--- blackjackgame.py
+++ blackjackgame.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Card:
     def __init__(self, suit, val):
         self.suit = suit
@@ -7,25 +8,40 @@ class Card:
         return
 
     def show(self):
-        return f'{self.val} of {self.suit}'
+        return f"{self.val} of {self.suit}"
 
 
 class Deck:
     def __init__(self):
         self.cards = []
         self.build()
-    
+
     def build(self):
-        for i in ['Hearts', 'Clubs', 'Spades', 'Diamonds']:
-            for n in ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']:
+        for i in ["Hearts", "Clubs", "Spades", "Diamonds"]:
+            for n in [
+                "Ace",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "Jack",
+                "Queen",
+                "King",
+            ]:
                 self.cards.append(Card(i, n))
         random.shuffle(self.cards)
-    
+
     def show(self):
         return [i.show() for i in self.cards]
 
+
 class Deal:
-    def __init__(self, deck, hand = []):
+    def __init__(self, deck, hand=[]):
         self.deck = deck
         self.hand = hand
 
@@ -33,11 +49,12 @@ class Deal:
         hand = self.deck[0:2]
         del self.deck[0:2]
         return hand
-    
+
     def hit(self):
         hand.append(self.deck[0])
         del self.deck[0]
         return hand
+
 
 class Score:
     def __init__(self, hand_cards):
@@ -51,9 +68,9 @@ class Score:
                     if int(n[i][0:2]) == 10:
                         n[i] = 10
                     elif isinstance(int(n[i][0]), int):
-                        n[i] = int(n[i][0])     
+                        n[i] = int(n[i][0])
                 except:
-                    if n[i][0] =='K' or n[i][0] == 'Q' or n[i][0] == 'J':
+                    if n[i][0] == "K" or n[i][0] == "Q" or n[i][0] == "J":
                         n[i] = 10
                     else:
                         n[i] = 11
@@ -67,8 +84,9 @@ class Score:
 
         return n
 
-class Player():
-    def __intit__(self, full_deck = None, hand = None):
+
+class Player:
+    def __intit__(self, full_deck=None, hand=None):
         if full_deck is None:
             full_deck = []
         self.full_deck = full_deck
@@ -101,7 +119,9 @@ class Dealer(Player):
 player_choice = 0
 while player_choice != 2:
     try:
-        player_choice = int(input('''
+        player_choice = int(
+            input(
+                """
       BLACKJACK
  ________    ________   
 |A       |  |J       |
@@ -112,75 +132,88 @@ while player_choice != 2:
 What would you like to do? Numerical input only!
     1. New Game
     2. Exit
-    '''
-    ))
+    """
+            )
+        )
         if player_choice == 1:
             deck = Player().new_game()
-            print('We are shuffled up! lets play')
+            print("We are shuffled up! lets play")
             play_menu = 0
             while play_menu != 4:
-                play_menu = int(input('''
+                play_menu = int(
+                    input(
+                        """
     1. Deal new hand
     2. Hit
     3. Call
     4. Exit to main menu
-    '''  ))
+    """
+                    )
+                )
                 if play_menu == 1:
                     if len(deck) < 4:
                         deck = Player().new_game()
-                        print('Reshuffling...')
+                        print("Reshuffling...")
                     hand = Player().deal()
-                    print('Your hand is...')
+                    print("Your hand is...")
                     for i in hand:
                         print(i)
-                    print(f'Your score is {sum(Score(hand).score_hand())}')
+                    print(f"Your score is {sum(Score(hand).score_hand())}")
                     dealer_hand = Dealer().deal()
-                    print(f'Dealer is showing a {dealer_hand[0]}')
+                    print(f"Dealer is showing a {dealer_hand[0]}")
                 elif play_menu == 2:
                     if len(deck) < 1:
                         deck = Player().new_game()
-                        print('Reshuffling...')
-                    if len(hand) <2:
+                        print("Reshuffling...")
+                    if len(hand) < 2:
                         print("you have to deal before you can hit")
                     else:
                         hand = Player().hit()
-                        print('Your hand is...')
+                        print("Your hand is...")
                         for i in hand:
                             print(i)
                         if sum(Score(hand).score_hand()) < 22:
-                            print(f'Your score is {sum(Score(hand).score_hand())}')
+                            print(f"Your score is {sum(Score(hand).score_hand())}")
                         else:
-                            print(f'Your score is {sum(Score(hand).score_hand())}')
-                            print('Ya Busted!')
+                            print(f"Your score is {sum(Score(hand).score_hand())}")
+                            print("Ya Busted!")
                             play_menu = 5
                 elif play_menu == 3:
-                    print(f'Your score is {sum(Score(hand).score_hand())}')
+                    print(f"Your score is {sum(Score(hand).score_hand())}")
                     player_hand = hand
                     hand = dealer_hand
                     print("Dealer's Turn!")
                     Dealer().hit()
-                    print(f'Dealer score is {sum(Score(hand).score_hand())}')
-                    if (sum(Score(hand).score_hand()) >= sum(Score(player_hand).score_hand())) and (sum(Score(hand).score_hand()) <= 21):
-                        print('Dealer Wins')
+                    print(f"Dealer score is {sum(Score(hand).score_hand())}")
+                    if (
+                        sum(Score(hand).score_hand())
+                        >= sum(Score(player_hand).score_hand())
+                    ) and (sum(Score(hand).score_hand()) <= 21):
+                        print("Dealer Wins")
                     else:
-                        print('You win!')
+                        print("You win!")
                     play_menu = 5
 
                 if play_menu == 5:
                     hand = []
-                    print('Would you like to try again?')
-                    play_again = int(input('''
+                    print("Would you like to try again?")
+                    play_again = int(
+                        input(
+                            """
     1. Yes
     2. no
-    '''))
+    """
+                        )
+                    )
                     if play_again == 2:
                         player_choice = 0
                         play_menu = 4
-                
+
                 if play_menu > 5:
                     play_menu = "fourty two"
     except:
-        print('''
+        print(
+            """
 What did you do!?!?! Type a letter instead of a number?
 Did you try to hit or call before dealing your starting hand?
 Did you write out a really long number like 42? It isn't always the answer you know!
@@ -188,5 +221,6 @@ Did you write out a really long number like 42? It isn't always the answer you k
 Either way, you would lose 500 points if there were points in this game. For shame!
 
 Back to the main menu to try again!
-            ''')
+            """
+        )
         player_choice = 0
